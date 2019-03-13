387b56afdff9c53fd8b459c35d39d788702ee43c
Korisnik <Korisnik@DESKTOP-M0DTDT2>
2019-03-13 20:43:39 +0100

    Kmit mrđa

---
+++ UnitsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("275dd56f-6d22-42dc-8215-d9fef0fc65e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitsTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom.Battleship.Model;
+
+namespace Vsite.Oom.Battleship.UnitsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConstructorCreatesSquareWithCoordinatesProvided()
+        {
+            Square s = new Square(2,3);
+            Assert.AreEqual(2, s.Row);
+            Assert.AreEqual(3, s.Column);
+        }
+    }
+}
