98af9c7cb17adb61ed93bf1e98c88a6af9cf1239
Mario Habijanec <mario.habijanec1@gmail.com>
2019-03-13 20:41:30 +0100

    added GetAvailablePlacements checks and tests

---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom.Battleship.Model;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Vsite.Oom.Battleship.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsListOf27ArraysForGrid5x6AndShip4()
+        {
+            Grid g = new Grid(5, 6);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsListOf27ArraysForGrid5x6AndShip4AfterEliminatingField2_2()
+        {
+            Grid g = new Grid(5, 6);
+            var squares = new List<Square> { new Square(2, 2) };
+            g.EliminateSquares(squares);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(22, arr.Count());
+        }
+
+    }
+}
