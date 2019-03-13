4778fcad4c04b1919dad274a354cb9b6d48a7b44
Ante Mišković <ante.miskovic@vsite.hr>
2019-03-13 20:41:58 +0100

    Add Model and UnitTests projects. Add squares.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a53774c-0f27-4423-b6f2-5691af66e4dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom.Battleship.Model;
+
+namespace Vsite.Oom.Battleship.UnitTests
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void ConstructorCreatesSquareWitdCoordinatesProvided()
+        {
+            Square s = new Square(2, 3);
+            Assert.AreEqual(2, s.Row);
+            Assert.AreEqual(3, s.Column);
+        }
+    }
+}
