eb13d1c21c1502e47b75d7dee8a29df7947a3557
Andro Bošnjak <andro.bosnjak172@gmail.com>
2019-03-13 21:37:08 +0100

    Napravili sve za drugu vjezbu

---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Vsite.Oom.Battleship.Model;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Vsite.Oom.Battleship.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetAvailablePlacementReturnesListOf27ArraysForGrid5x6AndShip4()
+        {
+            Grid g = new Grid(5, 6);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementReturnesListOf27ArraysForGrid5x6AndShip4AfterEliminatingField2_2()
+        {
+            Grid g = new Grid(5, 6);
+            g.EliminateSquares(new List<Square> { new Square(2, 2) });
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+        }
+    }
+}
