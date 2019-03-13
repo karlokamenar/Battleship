a622a48dc1a23e7eb4b9594e9558c205037008bc
Filip Kreč <filip.krec@yahoo.com>
2019-03-13 21:41:37 +0100

    Insert GetAvailablePlacements , Delete test

---
+++ UnitTest/TestGrid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom.Battleship.Model;
+using System.Linq;
+namespace Vsite.Oom.Battleship.UnitTest
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsListOf27ArraysForGrid5x6()
+        {
+            Grid g = new Grid(5, 6);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+            
+        }
+
+
+        public void GetAvailablePlacementsReturnsListOf27ArraysForGrid5x6AfterEliminatingField2_2()
+        {
+
+            Grid g = new Grid(5, 6);
+            var squares = new List<Square>{ new Square(2, 2) };
+            g.EliminateSquares(squares);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+
+        }
+    }
+}
