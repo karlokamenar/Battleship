41f782907ea3181f020d486d5d136c1c74204daf
mpiskac <mpiskac@gmail.com>
2019-03-13 21:38:46 +0100

    Add TestGrid Add TestSquare

---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Vsite.Oom.Battleship.Model;
+
+namespace Vsite.Oom.Battleship.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsListOf27ArraysForGrid5x6AndShip4()
+        {
+            Grid g = new Grid(5, 6);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+        }
+
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsListOf22ArraysForGrid5x6AndShip4AfterEliminatingField2_2()
+        {
+            Grid g = new Grid(5, 6);
+            var squares = new List<Square> { new Square(2, 2) };
+            g.EliminateSquares(squares);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+        }
+    }
+}
