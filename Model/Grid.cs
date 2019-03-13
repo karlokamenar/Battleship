da626ff75ba099b15671ab4a8ef7640ce495501a
Ante Mišković <ante.miskovic@vsite.hr>
2019-03-13 21:47:42 +0100

    *Add Grid

---
+++ Model/Grid.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Grid
+    {
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            squares = new Square[rows, columns];
+            for (int r = 0; r < rows; ++r)
+            {
+                for (int c = 0; c < columns; ++c)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int shipLength)
+        {
+            List<IEnumerable<Square>> list = GetHorizontalPlacements(shipLength);
+            list.AddRange(GetVerticalPlacements(shipLength));
+            return list;
+        }
+
+
+        public void EliminateSquares(IEnumerable<Square> squares)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private List<IEnumerable<Square>> GetHorizontalPlacements(int length)
+        {
+
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; ++r)
+            {
+                int count = 0;
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++count;
+                        if (count >= length)
+                        {
+                            List<Square> arr = new List<Square>();
+                            for (int c1 = c - length + 1; c1 <= c; ++c1)
+                                arr.Add(squares[r, c1]);
+                            result.Add(arr);
+
+                        }
+                    }
+                    else
+                        count = 0;
+                }
+            }
+            return result;
+        }
+
+
+
+        private List<IEnumerable<Square>> GetVerticalPlacements(int length)
+        {
+
+
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; ++c)
+            {
+                int count = 0;
+                for (int r = 0; r < Rows; ++r)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++count;
+                        if (count >= length)
+                        {
+                            List<Square> arr = new List<Square>();
+                            for (int r1 = r - length + 1; r1 <= r; ++r1)
+
+                                arr.Add(squares[r1, c]);
+                            result.Add(arr);
+
+                        }
+                    }
+                    else
+                        count = 0;
+                }
+            }
+            return result;
+        }
+
+
+        public readonly int Rows;
+        public readonly int Columns;
+        private Square[,] squares;
+    }
+}
---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom.Battleship.Model;
+namespace Vsite.Oom.Battleship.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsListOf27ArraysForGrid5x6AndShip4()
+        {
+            Grid g = new Grid(5,6);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+        }
+        [TestMethod]
+        public void GetAvailablePlacementsReturnsListOf22ArraysForGrid5x6AndShip4AfterEliminatingField2_2()
+        {
+            Grid g = new Grid(5, 6);
+            var squares= new List<Square> { new Square(2, 2) };
+            g.EliminateSquares(squares);
+            var arr = g.GetAvailablePlacements(4);
+            Assert.AreEqual(27, arr.Count());
+        }
+    }
+}
