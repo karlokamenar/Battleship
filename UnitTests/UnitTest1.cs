1f36166c6265d3eb97c0ddf0891d5781344b47fa
Matija Akrap <matija.akrap@gmail.com>
2019-03-13 20:41:44 +0100

    Add model and unit tests projects, Add Square

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Vsite.Oom.Battleship.Model;
+
+namespace Vsite.Oom.Battleship.UnitTests
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void ConstructorCreatesSquareWitdhCoordinatesProvided()
+        {
+            Square s = new Square(2, 3);
+            Assert.AreEqual(2, s.Row);
+            Assert.AreEqual(2, s.Column);
+        }
+    }
+}
